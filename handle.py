68bce78ec39b9d9a8d5a5394c5eb1707b3f8420d
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-03 10:46:33 +0800

    sss

--- handle.py
+++ handle.py
@@ -14,22 +14,6 @@ class Handle(object):
             data = web.input()
             if len(data) == 0:
                 return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = "galaxy" #请按照公众平台官网\基本配置中信息填写
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
-            else:
-                return ""
         except Exception, Argument:
             return Argument
 
@@ -41,31 +25,8 @@ class Handle(object):
             recMsg = receive.parse_xml(webData)
 
             if isinstance(recMsg, receive.Msg):
-
-                toUser = recMsg.FromUserName
-                fromUser = recMsg.ToUserName
-
-                if recMsg.MsgType == 'text':
-                    Logger().Log(Text.TEXT32.format(recMsg.Content))
-                    content = recMsg.Content
-                    replyMsg = reply.TextMsg(toUser, fromUser, content)
-                    return replyMsg.send()
-
-                if recMsg.MsgType == 'image':
-                    mediaId = recMsg.MediaId
-                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
-                    return replyMsg.send()
-
-                if recMsg.MsgType == 'event':
-                    Logger().Log(Text.TEXT33.format(recMsg.Event)) 
-                    event = recMsg.Event
-                    eventkey = recMsg.EventKey
-                    replyMsg = reply.EventMsg(toUser,fromUser,event,eventkey)
-                    return replyMsg.send()
-                else:
-                    return reply.Msg().send()
+		print "xxx"
             else:
                 print "暂且不处理"
-                return reply.Msg().send()
         except Exception, Argment:
             return Argment
--- receive.py
+++ receive.py
@@ -11,10 +11,6 @@ def parse_xml(web_data):
    
     if msg_type == 'text':
         return TextMsg(xmlData)
-    elif msg_type == 'image':
-        return ImageMsg(xmlData)
-    elif msg_type == 'event':
-        return EventMsg(xmlData)
 
 class Msg(object):
     def __init__(self, xmlData):
@@ -44,16 +40,3 @@ class Msg(object):
 class TextMsg(Msg):
     def __init__(self, xmlData):
         Msg.__init__(self, xmlData)
-        self.Content = xmlData.find('Content').text.encode("utf-8")
-
-class ImageMsg(Msg):
-    def __init__(self, xmlData):
-        Msg.__init__(self, xmlData)
-        self.PicUrl = xmlData.find('PicUrl').text
-        self.MediaId = xmlData.find('MediaId').text
-
-class  EventMsg(Msg):
-    def __init__(self,xmlData):
-        Msg.__init__(self, xmlData)
-        self.Event = xmlData.find("Event").text
-        self.EventKey = xmlData.find("EventKey").text
