293173fa3ef406ccb86e04a6011001933ad29432
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-08-14 16:22:22 +0800

    commit

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ if __name__ == '__main__':
     BlockMgr().Instance().Start()
     VoteMgr().Instance().regProducer("accountnum11",1)
     VoteMgr().Instance().regProducer("accountnum22",1)
+    VoteMgr().Instance().bwAction("proxyproxy11","proxyproxy11",100000,0)
     VoteMgr().Instance().bwAction("vote11111111","accountnum11",400000,0)
     VoteMgr().Instance().bwAction("vote11111122","accountnum22",600000,0) 
     VoteMgr().Instance().unbwAction("vote11111111",300000)
